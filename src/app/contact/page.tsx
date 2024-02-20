1eb1cf3028a13b3a3111e79fd566967429478702
eldani13 <ll-luis123@hotmail.com>
2024-02-20 11:18:58 -0500

    Primera parte

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function contact(){
+    return(
+        <div>
+            {/* Danna */}
+            <h1>Danna</h1>
+        </div>
+    )
+}
---
+++ src/app/home/page.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function inicio (){
+    return (
+        <div>
+            {/* Daniel */}
+            <h1>Daniel</h1>
+        </div>
+    )
+}
